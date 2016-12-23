ba2a2b51205445cdea24587731cdad4280afd479
Chris Breen <chris@kurl.io>
2016-12-23 10:14:36 +0000

    Uncommented php alias.

--- php/aliases.sh
+++ php/aliases.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-# alias php='php -dzend_extension="/usr/local/opt/php56-xdebug/xdebug.so"'
+# TODO this needs some work to get xdebug to play nice with php unit etc
+alias php='php -dzend_extension="/usr/local/opt/php56-xdebug/xdebug.so"'
+# alias composer='$(which php) $(which composer)'
 # alias phpunit='php $(which phpunit)'
