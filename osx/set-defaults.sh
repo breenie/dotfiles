c2bf8ac3d73abea81d413f994041f046f8ab4a70
Chris Breen <chris@kurl.co.uk>
2017-04-04 10:52:45 +0100

    Updated screenshot location.

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -44,4 +44,4 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-defaults write com.apple.screencapture location ~/Dropbox/photos/screenshots
+defaults write com.apple.screencapture location ~/Drive/photos/screenshots
