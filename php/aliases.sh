c4f650aa58b947c378c26431f0cb68e5b6c093bd
Chris Breen <chris@kurl.co.uk>
2018-06-04 10:35:59 +0100

    Corrected php alias for 1.5 brew.

--- php/aliases.sh
+++ php/aliases.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-# TODO this needs some work to get xdebug to play nice with php unit etc
-alias php='php -dzend_extension="/usr/local/opt/php56-xdebug/xdebug.so"'
-# alias composer='$(which php) $(which composer)'
-# alias phpunit='php $(which phpunit)'
-
-# export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_port=9000 remote_host=127.0.0.1 remote_connect_back=0"
+alias php='php -dzend_extension="$(pecl config-get ext_dir)/xdebug.so" -dxdebug.remote_autostart=1 -dxdebug.remote_start=1 -dxdebug.remote_connect_back=0 -dxdebug.remote_enable=1 -dxdebug.remote_host="localhost"'
