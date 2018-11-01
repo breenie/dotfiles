6f472bb437825ace14ce8d59f13778794db4180e
Chris <chris@kurl.io>
2018-11-01 10:33:17 +0000

    Added autocompletes.

--- bash/completion.sh
+++ bash/completion.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-# If possible, add tab completion
-AUTOCOMPLETE="$(brew --prefix)/etc/bash_completion"
+# Any errors caused by sed or complete options are probably the wrong shell.
+# sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+# chsh -s /usr/local/bin/bash
 
-[ -f "${AUTOCOMPLETE}" ] && . "${AUTOCOMPLETE}"
+# If possible, add tab completion
+AUTOCOMPLETE="$(brew --prefix)/share/bash-completion/bash_completion"
+
+if [ -f "${AUTOCOMPLETE}" ]; then
+  . "${AUTOCOMPLETE}"
+fi
