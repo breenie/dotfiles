fd649dbfe331a4de0e8df7334abcc5e55385b231
Chris <chris@kurl.io>
2018-11-01 10:31:20 +0000

    💄Forced light mode

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -45,3 +45,4 @@ defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool 
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 defaults write com.apple.screencapture location ~/Dropbox/photos/screenshots
+# defaults write -g NSRequiresAquaSystemAppearance -bool Yes
