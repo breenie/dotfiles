fd0892a7bc8e256b9c746a38317935ec68c77a86
Chris Breen <chris@kurl.co.uk>
2019-07-30 15:59:50 +0100

    🔥Removed dokku.

--- dokku/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Set a default dokku hostname
-#export DOKKU_HOST=dokku.kurl.io
--- dokku/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ ! -d "$HOME/.dokku" ]; then
-    git clone git@github.com:dokku/dokku.git ${HOME}/.dokku
-fi
